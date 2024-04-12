a833a0ef8b33d19039622ad80c3cc109ee233829
huangbh <578092215@qq.com>
2024-04-12 11:40:06 +0800

    feat(other): :sparkles: 使用pinia作为全局状态管理工具

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import '@unocss/reset/sanitize/assets.css';
 import './assets/style/public.css';
 import './assets/style/transition.css';
 import { setupRouter } from './router';
+import { setupStore } from './store';
 
 const initApp = async () => {
   const app = createApp(App);
   setupRouter(app);
+  setupStore(app);
   app.mount('#app');
 };
 
